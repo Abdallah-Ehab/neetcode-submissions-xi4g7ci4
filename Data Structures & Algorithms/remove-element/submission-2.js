cd2598ef39ea096acc22ceb88d08fd0591577f7e
Abdallah Ehab <148347769+Abdallah-Ehab@users.noreply.github.com>
2026-04-07 20:22:12 +0200

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let cur = 0;
+        for(let i = 0; i < nums.length; ++i){
+            if(nums[i] === val) continue;
+            nums[cur++] = nums[i]
+            
+        }
+        return cur
+    }
+}

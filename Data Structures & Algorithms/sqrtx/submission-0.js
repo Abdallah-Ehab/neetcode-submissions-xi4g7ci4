ab305799da07fcf56764ff0653bfe157c3b25075
Abdallah Ehab <148347769+Abdallah-Ehab@users.noreply.github.com>
2026-04-12 13:20:15 +0200

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    mySqrt(x) {
+        let l = 0;
+        let r = x;
+        while(l <= r){
+            let mid = Math.floor((l + r) / 2);
+            if(mid * mid === x){
+                return mid;
+            }else if(mid * mid < x){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+        return r;
+    }
+}

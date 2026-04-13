e175290bc814cd8a23c58343a5ba697cc628e796
Abdallah Ehab <148347769+Abdallah-Ehab@users.noreply.github.com>
2026-04-13 23:29:29 +0200

    Update: subsets-ii - submission-0

--- Data Structures & Algorithms/subsets-ii/submission-0.js
+++ Data Structures & Algorithms/subsets-ii/submission-0.js
@@ -3,19 +3,30 @@ class Solution {
      * @param {number[]} nums
      * @return {number[][]}
      */
-    permute(nums) {
-        let perms = [[]]
-        for(var n of nums){
-            let new_perms = []
-            for(var p of perms){
-                for(let i = 0; i < p.length + 1; ++i){
-                    let p_copy = [...p];
-                    p_copy.splice(i,0,n);
-                    new_perms.push(p_copy);
-                }
+    subsetsWithDup(nums) {
+        let path = []
+        let res = []
+        nums.sort((a,b)=> a - b);
+        const dfs = (i)=>{
+            if(i === nums.length){
+                res.push([...path])
+                return
             }
-            perms = new_perms;
+            // all subsets that include nums[i]
+            path.push(nums[i])
+            dfs(i + 1)
+            path.pop()
+
+            // all subsets that don't include nums[i]
+            while(i + 1 < nums.length && nums[i] === nums[i + 1])
+            {
+                i++;
+            }
+            dfs(i + 1)
+
+
         }
-        return perms;
+        dfs(0)
+        return res;
     }
 }
